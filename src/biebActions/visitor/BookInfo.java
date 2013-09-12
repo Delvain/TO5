8ee7851ea014a2a621bd6d10e76b8d2e6c7bb418
Jonathan <jonathankarssen@gmail.com>
2013-09-12 12:40:16 +0200

    lol, hacks

---
+++ src/biebActions/visitor/BookInfo.java
@@ -0,0 +1,41 @@
+package biebActions.visitor;
+
+import biebDomain.Book;
+import biebService.IBiebService;
+import biebService.ServiceProvider;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class BookInfo extends ActionSupport {
+	private IBiebService ibs = ServiceProvider.getBiebService();
+	private Book book = new Book();
+	private int bookId;
+	
+	public String execute() {
+		book = ibs.getBookById(bookId);
+		return ActionSupport.SUCCESS;
+	}
+	
+	public void validate() {
+		if (! ibs.bookExists(bookId)) {
+			addFieldError("bookId", "boekId is onbekend");
+		}
+	}
+	
+	public int getBookId() {
+		return bookId;
+	}
+	
+	public void setBookId(int bi) {
+		bookId = bi;
+	}
+	
+	public Book getBook() {
+		return book;
+	}
+	
+	public void setBook(Book b) {
+		book = b;
+	}
+
+}
