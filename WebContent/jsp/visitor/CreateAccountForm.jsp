eaf633442e3b702491937ce69edb273f409984fc
JRDNMLNDZ <jrdnmlndz@msn.com>
2013-09-18 12:23:10 +0200

    registratie

--- WebContent/jsp/visitor/CreateAccountForm.jsp
+++ WebContent/jsp/visitor/CreateAccountForm.jsp
@@ -3,18 +3,21 @@
 <html>
 
 	<head>
-         <title>Een account aanmaken</title>
+         <title>Plarktmaats</title>
          <link rel="stylesheet" type="text/css" href="../css/plarktmaats.css" />
 	</head>
 
 	<body>
-	    <h4>Een account aanmaken.</h4> 	
+	    <h4>Maak een wachtwoord aan om toegang te krijgen tot Plarktmaats</h4> 	
 	    
 	    <hr />
 	    
 		<s:form action="CreateAccount">
-    	  <s:textfield name="username" label="Naam"/>
+    	  <s:textfield name="email" label="E-mailadres"/>
+    	  <s:textfield name="voornaam" label="Voornaam"/>
+    	  <s:textfield name="achternaam" label="Achternaam"/>
     	  <s:password name="password" label="Wachtwoord"/>
+    	  <s:password name="password2" label="Hehaal wachtwoord"/>
     	  <s:submit value="maak account aan"/>
 		</s:form>
 			
--- src/plarktmaatsActions/visitor/CreateAccount.java
+++ src/plarktmaatsActions/visitor/CreateAccount.java
@@ -9,6 +9,7 @@ public class CreateAccount extends ActionSupport {
 	private String voornaam;
 	private String achternaam;
 	private String password;
+	private String password2;
 
 	public String execute(){
 		//ibs.addMember(username,password);
@@ -18,7 +19,10 @@ public class CreateAccount extends ActionSupport {
 
 	public void validate(){
 		email = email.trim();
+		voornaam = voornaam.trim();
+		achternaam = achternaam.trim();
 		password = password.trim();
+		
 		
 		if (email.length() == 0 ){			
 			addFieldError( "email", "email is verplicht");
@@ -31,13 +35,17 @@ public class CreateAccount extends ActionSupport {
 		if (achternaam.length() == 0) {
 			addFieldError( "achternaam", "achternaam is verplicht");
 		}
-		
+	
 //		else if ( ibs.userExists(username ) ){		
 //			addFieldError("username", "gebruiker bestaat al");
 //		}
 		
-		if ( password.length() == 0 ){			
+		if (password.length() == 0 ) {			
 			addFieldError( "password", "wachtwoord is verplicht");
+		}
+		
+		if (!password.equals(password2)) {
+			addFieldError( "password2", "wachtwoorden komen niet overeen");
 		}
 	}
 	
@@ -49,11 +57,31 @@ public class CreateAccount extends ActionSupport {
 		this.email = email;
 	}
 	
+	public String getVoornaam() {
+		return voornaam;
+	}
+	
+	public void setVoornaam(String voornaam) {
+		this.voornaam = voornaam;
+	}
+	
+	public String getAchternaam() {
+		return achternaam;
+	}
+	               
+	public void setAchternaam(String achternaam) {
+		this.achternaam = achternaam;
+	}
+
 //	public String getPassword() {
 //		return password;
 //	}
 	
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public void setPassword2(String password2) {
+		this.password2 = password2;
 	}
 }
