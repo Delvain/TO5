fc2804b1ace30acddf8ac21bea2d27c9057b9161
Jonathan <jonathankarssen@gmail.com>
2013-10-02 21:03:50 +0200

    dao 9e zooi

--- src/plarktmaatsDAO/VeilingDAOImpl.java
+++ src/plarktmaatsDAO/VeilingDAOImpl.java
@@ -59,7 +59,7 @@ public class VeilingDAOImpl implements PlarktmaatsDAOInterface<Veiling> {
 	public Veiling read(String pk) {
 		Connection con = connect();
 		try {
-			PreparedStatement read = con.prepareStatement("SELECT * FROM "+ConnectionData.DATABASE+".\"VEILING\" WHERE naam = ?");
+			PreparedStatement read = con.prepareStatement("SELECT * FROM "+ConnectionData.DATABASE+".\"VEILINGEN\" WHERE naam = ?");
 			read.setString(1, pk);
 			ResultSet rs = read.executeQuery();
 			while(rs.next()) {
@@ -86,21 +86,46 @@ public class VeilingDAOImpl implements PlarktmaatsDAOInterface<Veiling> {
 	}
 
 	@Override
-	public void update(String pk, Veiling c) {
-//		String voornaam = c.getVoornaam();
-//		String achternaam = c.getAchternaam();
-//		String email = c.getEmail();
-//		String banknr = c.getBankNr();
-//		String query = 	"UPDATE \"STUD1626376\".\"VEILING\" ";
-//		query +=		"SET voornaam='"+voornaam+"',achternaam='"+achternaam+"',email='"+email+"',banknr='"+banknr+"' ";
-//		query +=		"WHERE naam = '"+pk+"'";
-//		Connection con = connect();
-//		try {
-//			con.createStatement().execute(query);
-//			con.close();
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		}
+	public void update(String pk, Veiling v) {
+		int id = v.getVeilingId();
+		String naam = v.getProductNaam();
+		String omschrijving = v.getProductOmschrijving();
+		int minbedrag = v.getMinBedrag();
+		Calendar eindtijd = v.getEindTijd();
+		//TODO Date goed omzetten voor sql
+		Date einddatum = null;
+		if (eindtijd != null) {
+			einddatum = new java.sql.Date(eindtijd.getTimeInMillis());
+		} else {
+			Calendar morgen = Calendar.getInstance();
+			morgen.add(Calendar.DAY_OF_MONTH, 1);
+			einddatum = new java.sql.Date(morgen.getTimeInMillis());
+		}
+		
+		String gebruikersNaam = v.getAanbieder().getGebruikersnaam();
+		String categorieNaam = v.getDeCategorie().getNaam();
+		
+		//TODO foto erin zetten als blob
+		//File image = new File(path);
+//		FileInputStream fis = new FileInputStream ( image );
+//
+//		String sql="insert into imgtst (username,image) values (?, ?)";
+//		pst=con.prepareStatement(sql);
+//
+//		pst.setString(1, user);
+//		pst.setBinaryStream (2, fis, (int) file.length() );
+		
+		
+		String query = 	"UPDATE \"STUD1630460\".\"VEILINGEN\" ";
+		query +=		"SET id='"+id+"',naam='"+naam+"',omschrijving='"+omschrijving+"',minbedrag='"+minbedrag+"',eindtijd= To_Date('"+eindtijd+"','yyyy-mm-dd'),gebruikers_gebruikersnaam='"+gebruikersNaam+"',categorieen_naam='"+categorieNaam+"',foto='"+null+"' ";
+		query +=		"WHERE id = '"+pk+"'";
+		Connection con = connect();
+		try {
+			con.createStatement().execute(query);
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
