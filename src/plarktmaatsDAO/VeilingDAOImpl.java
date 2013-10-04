0997e6af3e6c6590c35c91a6f5cbe8a07d00aeb9
Jonathan <jonathankarssen@gmail.com>
2013-10-04 15:34:29 +0200

    maarten, al je jsps zijn kapot

--- src/plarktmaatsDAO/VeilingDAOImpl.java
+++ src/plarktmaatsDAO/VeilingDAOImpl.java
@@ -17,8 +17,8 @@ import plarktmaatsDomein.Veiling;
 public class VeilingDAOImpl implements PlarktmaatsDAOInterface<Veiling> {
 
 	@Override
-	public void create(Veiling v) { 
-		
+	public void create(Veiling v) {
+
 		int id = v.getVeilingId();
 		String naam = v.getProductNaam();
 		String omschrijving = v.getProductOmschrijving();
@@ -32,21 +32,25 @@ public class VeilingDAOImpl implements PlarktmaatsDAOInterface<Veiling> {
 			morgen.add(Calendar.DAY_OF_MONTH, 1);
 			einddatum = new java.sql.Date(morgen.getTimeInMillis());
 		}
-		
+
 		String gebruikersNaam = v.getAanbieder().getGebruikersnaam();
 		String categorieNaam = v.getDeCategorie().getNaam();
-		
-		//TODO foto erin zetten als blob
-		//File image = new File(path);
-//		FileInputStream fis = new FileInputStream ( image );
-//
-//		String sql="insert into imgtst (username,image) values (?, ?)";
-//		pst=con.prepareStatement(sql);
-//
-//		pst.setString(1, user);
-//		pst.setBinaryStream (2, fis, (int) file.length() );
-		
-		String query = "INSERT INTO "+ConnectionData.DATABASE+".\"VEILINGEN\" VALUES ('"+id+"', '"+naam+"', '"+omschrijving+"', '"+minbedrag+"', To_Date('"+einddatum+"','yyyy-mm-dd'), '"+gebruikersNaam+"', '"+categorieNaam+"', '"+4371938+"')";
+
+		// TODO foto erin zetten als blob
+		// File image = new File(path);
+		// FileInputStream fis = new FileInputStream ( image );
+		//
+		// String sql="insert into imgtst (username,image) values (?, ?)";
+		// pst=con.prepareStatement(sql);
+		//
+		// pst.setString(1, user);
+		// pst.setBinaryStream (2, fis, (int) file.length() );
+
+		String query = "INSERT INTO " + ConnectionData.DATABASE
+				+ ".\"VEILINGEN\" VALUES ('" + id + "', '" + naam + "', '"
+				+ omschrijving + "', '" + minbedrag + "', To_Date('"
+				+ einddatum + "','yyyy-mm-dd'), '" + gebruikersNaam + "', '"
+				+ categorieNaam + "', '" + 4371938 + "')";
 		Connection con = connect();
 		try {
 			con.createStatement().execute(query);
@@ -60,30 +64,36 @@ public class VeilingDAOImpl implements PlarktmaatsDAOInterface<Veiling> {
 	public Veiling read(String pk) {
 		Connection con = connect();
 		try {
-			PreparedStatement read = con.prepareStatement("SELECT * FROM "+ConnectionData.DATABASE+".\"VEILINGEN\" WHERE ID = ?");
+			PreparedStatement read = con.prepareStatement("SELECT * FROM "
+					+ ConnectionData.DATABASE + ".\"VEILINGEN\" WHERE ID = ?");
 			read.setString(1, pk);
 			ResultSet rs = read.executeQuery();
-			while(rs.next()) {
+			while (rs.next()) {
 				int id = rs.getInt("ID");
 				String naam = rs.getString("NAAM");
 				String omschrijving = rs.getString("OMSCHRIJVING");
 				int minbedrag = rs.getInt("MINBEDRAG");
-				Date eindtijdTemp = rs.getDate("EINDTIJD"); //rs.getDate("GEBDATUM");
+				Date eindtijdTemp = rs.getDate("EINDTIJD"); // rs.getDate("GEBDATUM");
 				Calendar eindtijd = Calendar.getInstance();
 				eindtijd.setTime(eindtijdTemp);
-				String gebruikersnaam = rs.getString("GEBRUIKERS_GEBRUIKERSNAAM");
+				String gebruikersnaam = rs
+						.getString("GEBRUIKERS_GEBRUIKERSNAAM");
 				String categorienaam = rs.getString("CATEGORIEEN_NAAM");
-	
-//				TODO foto toevoegen
-//				InputStream imgStream = resultSet.getBinaryStream(2);
-				
+
+				// TODO foto toevoegen
+				// InputStream imgStream = resultSet.getBinaryStream(2);
+
 				PersoonDAOImpl dao = new PersoonDAOImpl();
-				Gebruiker aanbieder = (Gebruiker)dao.read(gebruikersnaam);
+				Gebruiker aanbieder = (Gebruiker) dao.read(gebruikersnaam);
 				Categorie cat = new Categorie(categorienaam);
-				Veiling veil = new Veiling(id, naam, omschrijving, null, minbedrag, eindtijd, aanbieder, cat);
+				Veiling veil = new Veiling(id, naam, omschrijving, null,
+						minbedrag, eindtijd, aanbieder, cat);
 				BodDAOImpl boddao = new BodDAOImpl();
-				for (Bod b : boddao.getAllFromVeiling(id)) {
-					veil.voegBodToe(b);
+				ArrayList<Bod> biedingen = boddao.getAllFromVeiling(id);
+				if (!biedingen.isEmpty()) {
+					for (Bod b : biedingen) {
+						veil.voegBodToe(b);
+					}
 				}
 				return veil;
 			}
@@ -93,37 +103,40 @@ public class VeilingDAOImpl implements PlarktmaatsDAOInterface<Veiling> {
 		}
 		return null;
 	}
-	
+
 	public ArrayList<Veiling> getAll() {
 		ArrayList<Veiling> array = new ArrayList<Veiling>();
 		Connection con = connect();
 		try {
-			PreparedStatement read = con.prepareStatement("SELECT * FROM "+ConnectionData.DATABASE+".\"VEILINGEN\" ");
+			PreparedStatement read = con.prepareStatement("SELECT * FROM "
+					+ ConnectionData.DATABASE + ".\"VEILINGEN\" ");
 			ResultSet rs = read.executeQuery();
-			while(rs.next()) {
+			while (rs.next()) {
 				int id = rs.getInt("ID");
 				String naam = rs.getString("NAAM");
 				String omschrijving = rs.getString("OMSCHRIJVING");
 				int minbedrag = rs.getInt("MINBEDRAG");
-				Date eindtijdTemp = rs.getDate("EINDTIJD"); //rs.getDate("GEBDATUM");
+				Date eindtijdTemp = rs.getDate("EINDTIJD"); // rs.getDate("GEBDATUM");
 				Calendar eindtijd = Calendar.getInstance();
 				eindtijd.setTime(eindtijdTemp);
-				String gebruikersnaam = rs.getString("GEBRUIKERS_GEBRUIKERSNAAM");
+				String gebruikersnaam = rs
+						.getString("GEBRUIKERS_GEBRUIKERSNAAM");
 				String categorienaam = rs.getString("CATEGORIEEN_NAAM");
-				
-//				TODO foto toevoegen
-//				InputStream imgStream = resultSet.getBinaryStream(2);
-				
+
+				// TODO foto toevoegen
+				// InputStream imgStream = resultSet.getBinaryStream(2);
+
 				PersoonDAOImpl dao = new PersoonDAOImpl();
-				Gebruiker aanbieder = (Gebruiker)dao.read(gebruikersnaam);
+				Gebruiker aanbieder = (Gebruiker) dao.read(gebruikersnaam);
 				Categorie cat = new Categorie(categorienaam);
-				array.add(new Veiling(id, naam, omschrijving, null, minbedrag, eindtijd, aanbieder, cat));
+				array.add(new Veiling(id, naam, omschrijving, null, minbedrag,
+						eindtijd, aanbieder, cat));
 			}
 			con.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return array;
 	}
 
@@ -142,24 +155,28 @@ public class VeilingDAOImpl implements PlarktmaatsDAOInterface<Veiling> {
 			morgen.add(Calendar.DAY_OF_MONTH, 1);
 			einddatum = new java.sql.Date(morgen.getTimeInMillis());
 		}
-		
+
 		String gebruikersNaam = v.getAanbieder().getGebruikersnaam();
 		String categorieNaam = v.getDeCategorie().getNaam();
-		
-		//TODO foto erin zetten als blob
-		//File image = new File(path);
-//		FileInputStream fis = new FileInputStream ( image );
-//
-//		String sql="insert into imgtst (username,image) values (?, ?)";
-//		pst=con.prepareStatement(sql);
-//
-//		pst.setString(1, user);
-//		pst.setBinaryStream (2, fis, (int) file.length() );
-		
-		
-		String query = 	"UPDATE \"STUD1630460\".\"VEILINGEN\" ";
-		query +=		"SET id='"+id+"',naam='"+naam+"',omschrijving='"+omschrijving+"',minbedrag='"+minbedrag+"',eindtijd= To_Date('"+einddatum+"','yyyy-mm-dd'),gebruikers_gebruikersnaam='"+gebruikersNaam+"',categorieen_naam='"+categorieNaam+"',foto='"+78374+"' ";
-		query +=		"WHERE id = '"+pk+"'";
+
+		// TODO foto erin zetten als blob
+		// File image = new File(path);
+		// FileInputStream fis = new FileInputStream ( image );
+		//
+		// String sql="insert into imgtst (username,image) values (?, ?)";
+		// pst=con.prepareStatement(sql);
+		//
+		// pst.setString(1, user);
+		// pst.setBinaryStream (2, fis, (int) file.length() );
+
+		String query = "UPDATE \"STUD1630460\".\"VEILINGEN\" ";
+		query += "SET id='" + id + "',naam='" + naam + "',omschrijving='"
+				+ omschrijving + "',minbedrag='" + minbedrag
+				+ "',eindtijd= To_Date('" + einddatum
+				+ "','yyyy-mm-dd'),gebruikers_gebruikersnaam='"
+				+ gebruikersNaam + "',categorieen_naam='" + categorieNaam
+				+ "',foto='" + 78374 + "' ";
+		query += "WHERE id = '" + pk + "'";
 		Connection con = connect();
 		try {
 			con.createStatement().execute(query);
@@ -173,7 +190,8 @@ public class VeilingDAOImpl implements PlarktmaatsDAOInterface<Veiling> {
 	public void delete(String pk) {
 		Connection con = connect();
 		try {
-			PreparedStatement delete = con.prepareStatement("DELETE FROM "+ConnectionData.DATABASE+".\"VEILINGEN\" WHERE id = ?");
+			PreparedStatement delete = con.prepareStatement("DELETE FROM "
+					+ ConnectionData.DATABASE + ".\"VEILINGEN\" WHERE id = ?");
 			delete.setString(1, pk);
 			delete.execute();
 			con.close();
@@ -181,11 +199,12 @@ public class VeilingDAOImpl implements PlarktmaatsDAOInterface<Veiling> {
 			e.printStackTrace();
 		}
 	}
-	
-	private Connection connect() { //DONE
+
+	private Connection connect() { // DONE
 		try {
 			DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
-			return DriverManager.getConnection(ConnectionData.HOST, ConnectionData.USERNAME, ConnectionData.PASSWORD);
+			return DriverManager.getConnection(ConnectionData.HOST,
+					ConnectionData.USERNAME, ConnectionData.PASSWORD);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -193,18 +212,21 @@ public class VeilingDAOImpl implements PlarktmaatsDAOInterface<Veiling> {
 	}
 
 	public static void main(String[] args) {
-//		VeilingDAOImpl impl = new VeilingDAOImpl();
-//		Calendar gebdat = Calendar.getInstance();
-//		Gebruiker freak = new Gebruiker("Freak","Freek", "Nederland", "superloser@superfreak.com", gebdat, "8482929", "super");
-//		Categorie cat = new Categorie("Personen");
-//		Veiling veil = new Veiling(0, "freak", "superfreak original", null, 5, gebdat, freak, cat);
-////		impl.create(veil);
-////		veil = impl.read("freak");
-////		System.out.println(veil);
-////		veil = new Veiling("freak", "superfreak non-original", null, 5, gebdat, freak, cat);
-////		impl.update("0", veil);
-////		impl.delete("0");
-//		ArrayList<Veiling> array = impl.getAll();
-//		System.out.println(array.toString());
+		// VeilingDAOImpl impl = new VeilingDAOImpl();
+		// Calendar gebdat = Calendar.getInstance();
+		// Gebruiker freak = new Gebruiker("Freak","Freek", "Nederland",
+		// "superloser@superfreak.com", gebdat, "8482929", "super");
+		// Categorie cat = new Categorie("Personen");
+		// Veiling veil = new Veiling(0, "freak", "superfreak original", null,
+		// 5, gebdat, freak, cat);
+		// // impl.create(veil);
+		// // veil = impl.read("freak");
+		// // System.out.println(veil);
+		// // veil = new Veiling("freak", "superfreak non-original", null, 5,
+		// gebdat, freak, cat);
+		// // impl.update("0", veil);
+		// // impl.delete("0");
+		// ArrayList<Veiling> array = impl.getAll();
+		// System.out.println(array.toString());
 	}
 }
