0a650f643e9b4db8543443d10f39107cd4689820
Jonathan <jonathankarssen@gmail.com>
2013-10-30 12:23:45 +0100

    EIKELS

--- src/plarktmaatsActions/gebruikers/VoegVeilingToe.java
+++ src/plarktmaatsActions/gebruikers/VoegVeilingToe.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import plarktmaatsAware.UserAware;
 import plarktmaatsDAO.CategorieDAOImpl;
 import plarktmaatsDAO.VeilingDAOImpl;
+import plarktmaatsDomein.Beheerder;
 import plarktmaatsDomein.Categorie;
 import plarktmaatsDomein.Gebruiker;
 import plarktmaatsDomein.Persoon;
@@ -21,40 +22,62 @@ public class VoegVeilingToe extends ActionSupport implements UserAware {
 	private String minbedrag;
 	private Date eindDatum;
 	private Persoon user;
-	
-	
+
 	public String execute() {
 		System.out.println("voegveilingtoe execute");
 		Categorie cat = new Categorie(categorie);
-		img = null; //TODO foto toevoegen
-		int id = 0; //goede id wordt opgezocht in de database bij het insert-statement mbv een sequence
+		img = null; // TODO foto toevoegen
+		int id = 0; // goede id wordt opgezocht in de database bij het
+					// insert-statement mbv een sequence
 		Calendar eindtijd = Calendar.getInstance();
 		eindtijd.setTime(eindDatum);
 		Gebruiker gebruiker = (Gebruiker) user;
 		int min = Integer.parseInt(minbedrag);
-		Veiling veiling = new Veiling(id, productnaam, productomschrijving, img, min, eindtijd, gebruiker, cat);
+		Veiling veiling = new Veiling(id, productnaam, productomschrijving,
+				img, min, eindtijd, gebruiker, cat);
 		VeilingDAOImpl database = new VeilingDAOImpl();
 		database.create(veiling);
 		System.out.println("voegveilingtoe execute succes");
 		return SUCCESS;
 	}
-	
+
 	public void validate() {
 		System.out.println("voegveilingtoe validate");
-		//checken of categorie bestaat
+		// checken of categorie bestaat
 		CategorieDAOImpl categoriecheck = new CategorieDAOImpl();
 		Categorie cat = categoriecheck.read(categorie);
 		if (cat == null) {
 			addFieldError("categorie", "Deze categorie bestaat niet!");
 		}
-		//checken of minbedrag een getal is
+		// checken of minbedrag een getal is
 		try {
 			int min = Integer.parseInt(minbedrag);
 			if (min < 0) {
-				addFieldError("minbedrag","Minimumbod mag geen negatief getal zijn!");
+				addFieldError("minbedrag",
+						"Minimumbod mag geen negatief getal zijn!");
 			}
 		} catch (NumberFormatException nfe) {
-			addFieldError("minbedrag","Geen geldig minimumbod!");
+			addFieldError("minbedrag", "Geen geldig minimumbod!");
+		}
+		// checken of productnaam goed is
+		if (productnaam.equals("")) {
+			addFieldError("productnaam", "Productnaam mag niet leeg zijn!");
+		}
+		if (productnaam.length() > 35) {
+			addFieldError("productnaam",
+					"Productnaam mag maximaal 35 tekens zijn!");
+		}
+		// checken of productomschrijving goed is
+		if (productomschrijving.equals("")) {
+			addFieldError("productomschrijving", "Productomschrijving mag niet leeg zijn!");
+		}
+		if (productomschrijving.length() > 250) {
+			addFieldError("productomschrijving",
+					"Productomschrijving mag maximaal 250 tekens zijn!");
+		}
+		//checken of user geen beheerder is
+		if (user instanceof Beheerder) {
+			addFieldError("categorie","Als beheerder mag u geen veilingen toevoegen!");
 		}
 	}
 
