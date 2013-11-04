cde17b0dbda92cba0ffe9fbcd0d2b1d1045e93e7
Jonathan <jonathankarssen@gmail.com>
2013-11-04 22:43:29 +0100

    nutteloze klasse

--- src/plarktmaatsActions/beheerder/StatistiekMeesteBiedingen.java
+++
@@ -1,8 +0,0 @@
-package plarktmaatsActions.beheerder;
-
-import com.opensymphony.xwork2.ActionSupport;
-
-@SuppressWarnings("serial")
-public class StatistiekMeesteBiedingen extends ActionSupport {
-
-}
